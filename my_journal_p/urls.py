917c4500db3199ea7d7f555da9f6288dc8e81f75
Andy Larionov <andrewlarionov.urtk@gmail.com>
2022-04-10 10:08:33 +0500

    Добавление топиков

--- my_journal_p/urls.py
+++ my_journal_p/urls.py
@@ -6,5 +6,9 @@ from . import views
 app_name = 'my_journal_p'
 urlpatterns = [
     #Домашняя страница
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    #Страница со списками всех тем
+    path('topics/', views.topics, name='topics'),
+    #Страница с подробной информацией по отдельной теме
+    path('topics/<int:topic_id>/', views.topic, name='topic')
 ]
--- my_journal_p/views.py
+++ my_journal_p/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+from .models import Topic
 
 
 def index(request):
     return render(request, 'my_journal_p/index.html')
+
+
+def topics(request):
+    topics = Topic.objects.order_by('date_added')
+    context = {"topics": topics}
+    return render(request, 'my_journal_p/topics.html', context)
+
+
+def topic(request, topic_id):
+    topic = Topic.objects.get(id = topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic':topic, 'entries': entries}
+    return render(request, 'my_journal_p/topic.html', context)
+
 # Create your views here.
