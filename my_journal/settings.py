8ec699c050307fb6fbdfdc3429ba7adf0f46e1be
Andy Larionov <andrewlarionov.urtk@gmail.com>
2022-04-14 21:50:31 +0500

    Gjgsnrf

--- my_journal/settings.py
+++ my_journal/settings.py
@@ -127,5 +127,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = '/users/login'
 
 #Настройки Heroku
-import django_heroku
-django_heroku.settings(locals())
+# import django_heroku
+# django_heroku.settings(locals())
